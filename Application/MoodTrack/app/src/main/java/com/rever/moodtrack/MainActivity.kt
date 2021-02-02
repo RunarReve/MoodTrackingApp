afb24639e78166e3f551790ab75caeadced65dea
RunarReve <reverunar@outlook.com>
2021-02-02 12:41:53 +0000

    Set up the template Android application

---
+++ Application/MoodTrack/app/src/main/java/com/rever/moodtrack/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rever.moodtrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
