2eef2caeea9ba8b9b2004aca8ebf95b55353a436
RunarReve <reverunar@outlook.com>
2021-04-28 13:43:35 +0100

    Various changes

---
+++ Application/MoodTrack/app/src/main/java/com/rever/moodtrack/AreYouSureDialog.kt
@@ -0,0 +1,11 @@
+package com.rever.moodtrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AreYouSureDialog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_are_you_sure)
+    }
+}
--- Other/SyntheticGenerator/moveFile.sh
+++
@@ -1,3 +0,0 @@
-#Just a simple script to automatically move the generated file from 
-
-mv Data4Fab.kt ../Application/MoodTrack/app/src/main/java/com/rever/moodtrack/data/Fabricated/
